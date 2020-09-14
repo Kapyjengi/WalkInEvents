66b626452353f5dd69328f47c12734918ee3ff8f
Jukka Lehikoinen <57388263+JukkaLehikoinen@users.noreply.github.com>
2020-09-14 12:00:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,24 @@ function App() {
   const [longitude, setLongitude] = React.useState(0)
   const [latitude, setLatitude] = React.useState(0)
   const [loading, setLoading] = React.useState('LOADING')
+  const [lat, setLat] = useState()
+  const [lon, setLon] = useState()
+  const [area, setArea] = useState(1)
+  const [times, setTimes] = useState(0);
 
   let letEvent;
 
   // Sivun alkuu käytetään useEffectiä jossa ladataan lista kun sivu aukeaa
   useEffect(() => {
-    fetchData()    
-  }, [])
+    if (times < 5) {
+      Coords();
+      setTimes(times + 1)
+      }   
+  }, [times])
 
-  async function fetchData() {
-    let data = await fetch('https://cors-anywhere.herokuapp.com/http://open-api.myhelsinki.fi/v1/events/?limit=100', {
+  async function fetchData(lati,long,area) {
+    if (lati !== undefined && long  !== undefined){
+    let data = await fetch('https://cors-anywhere.herokuapp.com/http://open-api.myhelsinki.fi/v1/events/?distance_filter='+lati+'%2C'+long+'%2C'+area, {
 
       method: 'GET',
       headers: {
@@ -35,8 +43,19 @@ function App() {
     setEvents(data.data);
     console.log(data.data)
     setLoading('')
-    
+    }
   }
+
+   const Coords = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude)
+      setLon(position.coords.longitude)
+      });
+      let lati = lat
+      let long = lon
+      fetchData(lati,long,area);
+  }
+ 
 
   const ChangeDay = (event) => {
     setSelectedDay(event.target.value)
@@ -54,19 +73,24 @@ function App() {
     }
   }
 
-  const getMyPosition = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPosition)
+  
+  const AddArea = () =>{
+    setArea(area+1)
+    let aarea = area + 1
+    fetchData(lat,lon,aarea);
+  }
+
+  const SubArea = () =>{
+    if (area > 1){
+    setArea(area-1)
+    let aarea = area - 1
+    fetchData(lat,lon,aarea);
+    } else {
+      setArea(1)
+      let aarea = area
+      fetchData(lat,lon,aarea);
     }
   }
-
-  const getPosition = (position) =>  {
-    console.log('latitude is: ', position.coords.latitude)
-    console.log('longitude is: ', position.coords.longitude)
-    setLongitude(position.coords.longitude)
-    setLatitude(position.coords.latitude)
-  }
-
   const handleLongitudeChange = (event) => {
     console.log(event.target.value)
     setLongitude(event.target.value)
@@ -106,7 +130,7 @@ function App() {
         Latitude: <input type="text" id="Latitude" onChange={handleLatitudeChange} value={latitude}  />
         </p>
         <p><button onClick={ShowAll}>Show all</button></p>
-        <p><button onClick={getMyPosition}> Get my location</button></p>
+        <p><button onClick={AddArea}>+1km</button><button onClick={SubArea}>-1km</button></p>
         <Show events={events} event={event} selectedDay={selectedDay} longitude={longitude} latitude={latitude} />
       </div>
     )
