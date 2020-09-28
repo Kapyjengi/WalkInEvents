36744a413b0b6203686d5482dbf873a9bb52b776
Paivi Lampinen <bet983@myy.haaga-helia.fi>
2020-09-28 20:22:39 +0300

    EventCards with mock events

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -4,10 +4,41 @@ import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 
-const EventCard = () => {
+export default function EventCard() {
+
+    let mockEvents = ['One', 'Two', 'Three', 'Four']
+    let cardMockEvents = mockEvents.map((mockEvents, i) => {
+
+        let name = mockEvents;
+        let dateAndTime = 'date&time '+(i+1);
+        let place = 'place '+(i+1);
+        let address = 'address '+(i+1);
+        let distance = (i+1)*100+' m';
+        
+        return (
+          <Card key={i} style={{ marginTop:10 }} bg='light'>
+            <Card.Body>
+                <Row style={{ paddingBottom:20 }}>
+                    <Col>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>{dateAndTime}<br/>{place}<br/>{address}</Card.Text>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={6}>{distance}</Col>
+                    <Col xs={6}>
+                        <Button variant="primary" style={{ marginRight:10 }}>Show more</Button>
+                        <Button variant="secondary">WWW</Button>
+                    </Col> 
+                </Row>
+            </Card.Body>
+          </Card>
+        )
+    })
+
     return (
         <div>
-            <Card className="text-left">
+            <Card>
                 <Card.Body>
                     <Row style={{ paddingBottom:20 }}>
                         <Col xs={12} md={6}>
@@ -26,8 +57,7 @@ const EventCard = () => {
                     </Row>
                 </Card.Body>
             </Card>
+            {cardMockEvents}
         </div>
     )
 }
-
-export default EventCard
