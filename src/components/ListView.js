671d1917896e2e4a9bca2a07f38ff8806f92e7eb
Jukka Lehikoinen <57388263+JukkaLehikoinen@users.noreply.github.com>
2020-10-29 13:24:02 +0200

    Location Permission issue

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -6,10 +6,8 @@ import ShowTagOptions from './ShowTagOptions'
 import MapView from './MapView'
 import Filtteri from './Filter'
 import Fetch from './Fetch'
-import {useStore, useDispatch, connect} from 'react-redux'
-import {setUserLocation} from '../GlobalStore/locationActions'
-import GetAllEvents from '../LogicalFunctions/GetAllEvents'
-
+import { useStore, useDispatch, connect } from 'react-redux'
+import { setUserLocation } from '../GlobalStore/locationActions'
 
 export default function ListView() {
   const store = useStore()
@@ -22,37 +20,62 @@ export default function ListView() {
   const [loaded, setLoaded] = useState(false)
   const [lat, setLat] = useState()
   const [lon, setLon] = useState()
-  const [area, setArea] = useState(10)
+  const [area, setArea] = useState(1)
 
-  
+
 
   // Sivun alkuu käytetään useEffectiä jossa ladataan lista kun sivu aukeaa
   useEffect(() => {
     //Haetaan koordinaatit
     Coords();
   }, [lon, lat])
-  useEffect(()=> {
-    GetAllEvents();
-  }, [])
 
 
   async function fetchData(lati, long, area) {
-    let data=(Fetch(lati,long,area))
-    // kutsutaan funktiota, joka hakee kaikki eventit
+    let data = (Fetch(lati, long, area))
     setEvents((await data))
-    if (events !== ''){
-    setLoading('')
-    setLoaded(true)
+    if (events !== '') {
+      setLoading('')
+      setLoaded(true)
     }
+
   }
 
-  const Coords =() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude)
-      setLon(position.coords.longitude)
-      store.dispatch(setUserLocation(position.coords.latitude, position.coords.longitude))
-  
-    });
+  const Coords = () => {
+
+    let id, target, options;
+
+    function success(pos) {
+      let crd = pos.coords;
+      if (target.latitude !== crd.latitude && target.longitude !== crd.longitude) {
+        setLat(crd.latitude)
+        setLon(crd.longitude)
+        store.dispatch(setUserLocation(crd.latitude, crd.longitude))
+        navigator.geolocation.clearWatch(id);
+      }
+    }
+
+    function error(err) {
+      //console.warn('ERROR(' + err.code + '): ' + err.message);
+      setLat(60.1733244)
+      setLon(24.9410248)
+      store.dispatch(setUserLocation(lat, lon))
+    }
+
+    target = {
+      latitude: 0,
+      longitude: 0
+    };
+
+    options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    id = navigator.geolocation.watchPosition(success, error, options);
+
+
     let lati = lat
     let long = lon
     //Haetaan funktion kautta päivämäärä joka muutetaan isomuotoon.
@@ -61,26 +84,29 @@ export default function ListView() {
     //Haetaan API
     fetchData(lati, long, area);
   }
-  
-  const ShowFilters=(dayNew,eventNew,areaNew)=>{
-    areaNew=parseInt(areaNew)
+
+
+
+
+  const ShowFilters = (dayNew, eventNew, areaNew) => {
+    areaNew = parseInt(areaNew)
     console.log(eventNew)
     if (dayNew !== undefined) {
       setSelectedDay(dayNew)
     }
-    if (eventNew!==undefined) {
+    if (eventNew !== undefined) {
       setEvent(eventNew)
     }
-    if (area !== areaNew){
-      console.log(area,areaNew)
+    if (area !== areaNew) {
+      console.log(area, areaNew)
       setArea(areaNew)
-      const alueNew= areaNew
-      fetchData(lat, lon,alueNew)
+      const alueNew = areaNew
+      fetchData(lat, lon, alueNew)
       setLoading('LOADING')
     }
-    
+
   }
- 
+
   // Niin kauan kuin loading state on 'LOADING' niin näytetään pelkästään lataus 'merkkiä'
   if (loading === 'LOADING' && !loaded) {
     return (
@@ -94,8 +120,8 @@ export default function ListView() {
     return (
       <div className="App">
         <h1> </h1>
-        <Filtteri ShowFilters={ShowFilters} selectedDay={selectedDay} event={event}/>
-        <MapView latitude={lat} longitude={lon} events={events} event={event} area={area} selectedDay={selectedDay} ></MapView> 
+        <Filtteri ShowFilters={ShowFilters} selectedDay={selectedDay} event={event} />
+        <MapView latitude={lat} longitude={lon} events={events} event={event} area={area} selectedDay={selectedDay} ></MapView>
         <ShowTagOptions events={events} />
         area:{area}km
         <Loading loading={loading} loaded={loaded} />
