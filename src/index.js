f81c6ca109be6380dcccdafce7e4cdffb810be07
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-10-29 15:43:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './GlobalStore/store'
+import store from './GlobalStore/Store'
 
 
 ReactDOM.render(
