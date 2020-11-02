56a6e2e16f0966c5fe00cd022c7abbe51af40bc5
Jukka Lehikoinen <57388263+JukkaLehikoinen@users.noreply.github.com>
2020-11-02 13:27:51 +0200

    Update EventCards.js

--- src/SharedViewComponents/EventCards.js
+++ src/SharedViewComponents/EventCards.js
@@ -1,66 +1,77 @@
-import React, {useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import store from '../GlobalStore/Store'
 import { connect } from 'react-redux'
+import { useStore } from 'react-redux'
+import moment from 'moment'
+import L from 'leaflet'
 
 export default function EventCard() {
     connect()
+    
     //let events = store.getState().filteredEvents
+    //const [events, setEvents] = useState(store.getState().filteredEvents)
     const [events, setEvents] = useState(store.getState().filteredEvents)
+    const [location, setLocation] = useState([])
+    
+    useEffect(()=>{
+        printAllEvents()
+    })
+
+    console.log('store',store.getState().filteredEvents)
+    
+    const printAllEvents = () => {
+        setEvents(store.getState().filteredEvents)
+        setLocation(store.getState().userLocation)
+    }
 
 
-    let cardEvents = events.map((events, i) => {
+    let cardEvents = events.map((events, i)  => {
         let name = events.name.fi;
-        let dateAndTime = 'date&time '+(i+1);
-        //let place = 'place '+(i+1);
-        let address = 'address '+(i+1);
-        let distance = (i+1)*100+' m';
-        
+        let dateAndTime = moment(`${events.event_dates.starting_day}`).format("DD.MM.YYYY HH:mm")
+        let address = events.location.address.street_address + ', ' +
+            events.location.address.postal_code + ' ' +
+            events.location.address.locality;
+        let infoUrl = events.info_url;
+        let disable = false;
+        let buttonColor = "primary"
+        if (infoUrl === null) {
+            disable = true
+            buttonColor = "secondary"
+        }
+
+        let distance = L.latLng(location.latitude, location.longitude).distanceTo(L.latLng(events.location.lat, events.location.lon)).toFixed(0) + ' m';
+
+
         return (
-          <Card key={i} style={{ marginTop:10 }} bg='light'>
-            <Card.Body>
-                <Row style={{ paddingBottom:20 }}>
-                    <Col>
-                        <Card.Title>{name}</Card.Title>
-                        <Card.Text>{dateAndTime}<br/>{address}</Card.Text>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={6}>{distance}</Col>
-                    <Col xs={6}>
-                        <Button variant="primary" style={{ marginRight:10 }}>Show more</Button>
-                        <Button variant="secondary">WWW</Button>
-                    </Col> 
-                </Row>
-            </Card.Body>
-          </Card>
+            <Card key={i} style={{ marginTop: 10 }} bg='light'>
+                <Card.Body>
+                    <Row style={{ paddingBottom: 20 }}>
+                        <Col>
+                            <Card.Title>{name}</Card.Title>
+                            <Card.Text>{dateAndTime}<br />{address}</Card.Text>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}>{distance}</Col>
+                        <Col xs={6}>
+                            <Button variant="info" style={{ marginRight: 10 }}>Show more</Button>
+                            <Button href={infoUrl} target="_blank" variant={buttonColor} disabled={disable}>WWW</Button>
+
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
         )
     })
 
     return (
         <div>
-            <Card>
-                <Card.Body>
-                    <Row style={{ paddingBottom:20 }}>
-                        <Col xs={12} md={6}>
-                        <Card.Title>John Bishop Live</Card.Title>
-                        <Card.Text>
-                        20.10.2020 19:30<br/>Hall of Culture<br/>Sturenkatu 4 00510 Helsinki
-                        </Card.Text>
-                        </Col>
-                    </Row>   
-                    <Row>
-                        <Col xs={6}>300 m</Col>
-                        <Col xs={6}>
-                            <Button variant="primary" style={{ marginRight:10 }}>Show more</Button>
-                            <Button variant="secondary">WWW</Button>
-                        </Col> 
-                    </Row>
-                </Card.Body>
-            </Card>
+
+            <Button onClick={printAllEvents}>NAPPI ONNEEN</Button>
             {cardEvents}
         </div>
     )
