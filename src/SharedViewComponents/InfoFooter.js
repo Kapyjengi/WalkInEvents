aa54de073d957db07ace76cc7035827387216fbc
sami <bgh296@myy.haaga-helia.fi>
2020-11-12 16:13:21 +0200

    Separated toolbarfooter and infofooter
    Added datepicker and responsive functionalities

--- src/SharedViewComponents/InfoFooter.js
+++ src/SharedViewComponents/InfoFooter.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function InfoFooter() {
     return (
-        <div className="footer">
-            <p>This is sticky infofooter. Created by Käpy Oy
-            <br />Copyright (c) 2020 Walk-In-Events.</p>
+        <div className="infofooter">
+            <p>Copyright (c) 2020 Walk-In-Events.</p>
         </div>
     )
 }
--- src/SharedViewComponents/ToolbarFooter.js
+++ src/SharedViewComponents/ToolbarFooter.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Slider from '../SharedViewComponents/DistanceSlider'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setDate } from '../GlobalStore/TimeActions'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import InfoFooter from './InfoFooter'
+
+
 
 export default function ToolbarFooter() {
 
-    const range = useSelector(state => state.range)
+    const state = useSelector(state => state)
+    const range = state.range
+    const selectedDate = state.selectedDate
+    const dispatch = useDispatch()
 
     return (
-        <div className="footer">
-            Range: {range} km
+        <div className="toolbarfooter">
+            <Row>
+                <Col xs={12} md={8}>
+                    Range: {range} km
             <Slider />
-            <p>This is sticky toolbarfooter. Created by Käpy Oy
-            <br />Copyright (c) 2020 Walk-In-Events.</p>
+                </Col>
+                <Col xs={12} md={4}>
+                    <p>date: <input id="selectday" type="date" value={selectedDate} onChange={event => dispatch(setDate(event.target.value))} /></p>
+                </Col>
+            </Row>
+            <InfoFooter />
         </div>
     )
 }
