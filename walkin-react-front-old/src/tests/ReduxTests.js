a5918b9318f53b7d438afa02b6ef1a34fe550e82
AntonJ <anton.javits@gmail.com>
2020-11-16 00:38:35 +0200

    Deployment to Heroku
    Changes in package.json and fetch, old react-front removed

--- walkin-react-front-old/src/tests/ReduxTests.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import {useStore, useDispatch} from 'react-redux'
-
-export default function ReduxTests() {
-    const store = useStore()
-
-    console.log('Running redux tests')
-    
-}
