f107adb34e44c0ec1b646942fc6c78ceef386aa0
AntonJ <anton.javits@gmail.com>
2020-11-20 09:40:51 +0200

    Port to 3000

--- walkin-node-backend/index.js
+++ walkin-node-backend/index.js
@@ -36,7 +36,7 @@ app.get('/api/events', async (req, res) =>  {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
