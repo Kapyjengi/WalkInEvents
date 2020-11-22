0e3d4609763030b2cb06fc29c18de9c27532069d
AntonJ <anton.javits@gmail.com>
2020-11-22 08:45:06 +0200

    Add build-script to .yml
    Add version to footer for testing

--- react-front/src/SharedViewComponents/InfoFooter.js
+++ react-front/src/SharedViewComponents/InfoFooter.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function InfoFooter() {
     return (
         <div className="infofooter">
-            <p>Copyright (c) 2020 Walk-In-Events.</p>
+            <p>Copyright (c) 2020 Walk-In-Events. v:0.1</p>
         </div>
     )
 }
