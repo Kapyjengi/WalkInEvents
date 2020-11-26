cb31b1cc7388a39ac97221b71002522b2c8295f1
AntonJ <anton.javits@gmail.com>
2020-11-26 10:43:37 +0200

    Manual build for demo

---
+++ build/precache-manifest.07f9ab690b507f7505b22318dde915cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5b3901106902e3ce3f5084936f5daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a50fd77576767d7433f2",
+    "url": "/static/css/2.23bfdb06.chunk.css"
+  },
+  {
+    "revision": "979742d06e9cde567b6e",
+    "url": "/static/css/main.70b81239.chunk.css"
+  },
+  {
+    "revision": "a50fd77576767d7433f2",
+    "url": "/static/js/2.85eca7d1.chunk.js"
+  },
+  {
+    "revision": "772d225dec21a2717ccf6aba5887bee3",
+    "url": "/static/js/2.85eca7d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "979742d06e9cde567b6e",
+    "url": "/static/js/main.f5e855b4.chunk.js"
+  },
+  {
+    "revision": "2b4d8073df71b39be47a",
+    "url": "/static/js/runtime-main.6d871f28.js"
+  }
+]);
