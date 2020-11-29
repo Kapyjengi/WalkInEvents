ae244e4f2d7f5a30cb220e7fb185f136e998b3fe
AntonJ <anton.javits@gmail.com>
2020-11-29 20:47:09 +0200

    Naming and version

--- react-front/src/SharedViewComponents/InfoFooter.js
+++ react-front/src/SharedViewComponents/InfoFooter.js
@@ -5,7 +5,7 @@ export default function InfoFooter() {
     return (
       <Row>
         <Col className="infofooter">
-            <p>Copyright (c) 2020 Walk-In-Events. v:0.1.1</p>
+            <p>Copyright (c) 2020 Walk-In-Events. v:0.1.2</p>
         </Col>
       </Row>
     )
