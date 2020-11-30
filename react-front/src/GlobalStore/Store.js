c751c3855b7215cb1c229aa875131d58406404d3
sami <bgh296@myy.haaga-helia.fi>
2020-11-30 16:56:29 +0200

    Update Store.js
    deleted comment and unused import

--- react-front/src/GlobalStore/Store.js
+++ react-front/src/GlobalStore/Store.js
@@ -1,16 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import firstReducer from '../reducers'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-/* const reducer = combineReducers({
-    first: firstReducer
-  })
-  
-const store = createStore(reducer) */
-
 const store = createStore(
-    firstReducer,
-    composeWithDevTools()
+  firstReducer,
+  composeWithDevTools()
 )
 
 export default store
