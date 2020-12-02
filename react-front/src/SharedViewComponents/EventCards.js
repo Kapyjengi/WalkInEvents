d0055d06f2058058a962f71a29d95914d6d8f454
AntonJ <anton.javits@gmail.com>
2020-12-02 11:51:34 +0200

    EventCards: Scrollable container for description.body

--- react-front/src/SharedViewComponents/EventCards.js
+++ react-front/src/SharedViewComponents/EventCards.js
@@ -1,6 +1,6 @@
 import L from 'leaflet'
 import moment from 'moment'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
@@ -9,6 +9,7 @@ import { useSelector } from 'react-redux'
 import { setEventSearch } from '../GlobalStore/EventActions'
 import store from '../GlobalStore/Store'
 import RunEventFilters from '../LogicalFunctions/RunEventFilters'
+import parse from 'html-react-parser'
 
 export default function EventCard() {
 
@@ -16,6 +17,8 @@ export default function EventCard() {
     const userLocation = state.userLocation
     const location = { lat: userLocation.latitude, lng: userLocation.longitude }
     const filtered = state.filteredEvents
+    const [readMore, setReadMore] = useState(false);
+    const readMorelinkName = readMore ? 'Read Less << ' : 'Read More >> '
 
     const SeekName = (e) => {
         let keyWord = e.target.value
@@ -33,14 +36,30 @@ export default function EventCard() {
         }
         return nameToReturn
     }
+    function getDesc(event) {
+      let descToReturn = event.desc.fi
+      if (event.desc.fi === null) {
+          descToReturn = event.desc.sv;
+          if (event.desc.sv === null) {
+              descToReturn = event.desc.en
+          }
+      }
+      return descToReturn
+  }
 
     let cardEvents = filtered.map((events, i) => {
-
+        console.log("Events from map-function: ", events)
+        console.log("Events.description.intro from map-function: ", events.description.intro)
+        console.log("Events.description.body from map-function: ", events.description.body)
         let name = getName(events);
+        //let desc = getDesc(events);
         let dateAndTime = moment(`${events.event_dates.starting_day}`).format("DD.MM.YYYY HH:mm")
         let address = events.location.address.street_address + ', ' +
             events.location.address.postal_code + ' ' +
             events.location.address.locality;
+        //let desc = events.desc
+        // let fullDesc = parse(events.fullDesc)
+        let fullDesc = events.fullDesc
         let infoUrl = events.info_url;
         let disable = false;
         let buttonColor = "primary"
@@ -60,6 +79,14 @@ export default function EventCard() {
                           <Col>
                               <Card.Title>{name}</Card.Title>
                               <Card.Text>{dateAndTime}<br />{address}</Card.Text>
+                              <Card.Text>
+                                <h6>{events.description.intro}</h6> 
+                                <div className="card-body-scrollable">
+                                  
+                                  {parse(events.description.body)}
+                                
+                                </div>
+                             </Card.Text>
                           </Col>
                       </Row>
                       <Row>
