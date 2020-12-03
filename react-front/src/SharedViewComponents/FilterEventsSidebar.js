7d5530f15d34c9c82eeacfca73a1815c5e50b76e
sami <bgh296@myy.haaga-helia.fi>
2020-12-03 14:33:29 +0200

    Update FilterEventsSidebar.js
    deleted unused imports, consts and functions

--- react-front/src/SharedViewComponents/FilterEventsSidebar.js
+++ react-front/src/SharedViewComponents/FilterEventsSidebar.js
@@ -1,24 +1,13 @@
-import React, { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import React from 'react'
+import { Button } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
-import RunEventFilters from '../LogicalFunctions/RunEventFilters'
 import ShowTagOptions from './ShowTagOptions'
 import SwitchAllTagOptions from '../LogicalFunctions/SwitchAllTagChecks'
 
 export default function FilterEventsSidebar(props) {
-  const state = useSelector(state => state) 
-  const [open, setOpen] = useState(false);
+  const state = useSelector(state => state)
   const numberOfFilteredEvents = state.filteredEvents.length
-  
 
-  const handleClose = () => {
-    setOpen(false);
-    RunEventFilters()
-  }
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  }
   const checkOrUncheckAll = () => {
     SwitchAllTagOptions()
   }
@@ -28,7 +17,7 @@ export default function FilterEventsSidebar(props) {
       <p>Showing {numberOfFilteredEvents} events.</p>
       <ShowTagOptions />
       <Button variant="secondary" onClick={checkOrUncheckAll}>Check/Uncheck All</Button>
-    
+
     </div>
   )
 }
