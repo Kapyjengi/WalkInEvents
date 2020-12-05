6d4665d3d201388061a9db360a07f5bd74bea40d
sami <bgh296@myy.haaga-helia.fi>
2020-12-05 15:05:07 +0200

    Update About.test.js

--- react-front/src/ViewComponents/__tests__/About.test.js
+++ react-front/src/ViewComponents/__tests__/About.test.js
@@ -7,14 +7,11 @@ import { act } from "react-dom/test-utils";
 let container = null;
 
 beforeEach(() => {
-    // luodaan dom-elementti
     container = document.createElement("div");
-    // johon testi renderöidään
     document.body.appendChild(container);
 });
 
 afterEach(() => {
-    // testin jälkeen dom-elementti tyhjennetään
     unmountComponentAtNode(container);
     container.remove();
     container = null;
@@ -22,11 +19,10 @@ afterEach(() => {
 
 
 describe('About', () => {
-    it('renders the correct content with correct header', () => {
+    it('renders the component with correct header', () => {
         act(() => {
             render(<AboutView />, container)
         })
-        // about sivu renderöidään kokonaisuudessaan divin sisään
         const AboutHeader = "About Walk-In-Events"
         expect(container.textContent).toContain(AboutHeader)
     })
