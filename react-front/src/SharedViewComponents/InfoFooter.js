37c7e4bfd2cc5ce1964410eaae4c801c4a691a06
AntonJ <anton.javits@gmail.com>
2020-12-10 14:26:56 +0200

    Contact information in footer and About

--- react-front/src/SharedViewComponents/InfoFooter.js
+++ react-front/src/SharedViewComponents/InfoFooter.js
@@ -5,7 +5,8 @@ export default function InfoFooter() {
     return (
       <Row>
         <Col className="infofooter">
-            <p>Copyright &copy;2020 Walk-in Events. Licence: CC-BY</p>
+            <p>Copyright &copy;2020 Walk-in Events. Licence: CC-BY<br/>
+            <a href="walkinevents.hki@gmail.com">walkinevents.hki@gmail.com</a></p>
         </Col>
       </Row>
     )
--- react-front/src/ViewComponents/About.js
+++ react-front/src/ViewComponents/About.js
@@ -15,9 +15,8 @@ const AboutView = () => {
                   All the events and their information come from an open API, which is provided by the City of Helsinki.</p>
                   <p>All rights are reserved by Helsinki city's council. The license for all the information provided by Helsinki, is a BY-license. 
                   Walk-In-Events is an application, created by team Käpy, for a course at Haaga-Helia.</p>
-                  <p>Our application is built on open source data. The repository for this project is public. You can find the code and documentation from our: 
-                      <a href='https://github.com/Kapyjengi/WalkInEvents'> Github</a>.</p>
-                  <p></p>
+                  <p>Our application is built on open source data. The repository for this project is public. You can find the code and documentation from our <a href='https://github.com/Kapyjengi/WalkInEvents'>Github</a>.</p>
+                  <p>Contact us: <a href="walkinevents.hki@gmail.com">walkinevents.hki@gmail.com</a></p>
                 </Col>
               </Row>
             </Container>
